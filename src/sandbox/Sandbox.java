c0c2fc921ab6cfe7fdfe7dfd2333e5506cf2b11b
Josh Gomez <heyjersh@gmail.com>
2018-04-26 01:27:09 +0100

    Extra fun with loops

--- src/sandbox/Sandbox.java
+++ src/sandbox/Sandbox.java
@@ -43,6 +43,43 @@ public class Sandbox {
         Sandbox sandboxOne = new Sandbox("First Java Sandbox");
         sandboxOne.setLevel(1);
         sandboxOne.printName();
+        
+        //More experimentation
+        System.out.println("Now that THAT stuff is outta the way, let's do something cool");
+        System.out.println("Are you a professional engineer of some sort?");
+        
+        // Fun with Loops
+        int x = 1;
+        
+        //WHILE
+        while (x <= 10) {
+            System.out.println("x is: " + x);
+            x++;
+        }
+        
+        //FOR
+        for(int y = 10; y <= 20; y++) {
+            System.out.println("Value of y : " + y );
+        }
+        
+        //DO WHILE
+        int z = 2;
+        do {
+            System.out.println("Value of Z: " + z);
+            z++;
+        }while(z <= 10);
+        
+        //Enhanced For
+        int [] numbers = {10, 20, 30, 40, 50};
+        
+        for (int q : numbers) {
+            if (q == 30) {
+                continue;
+            }
+            System.out.println(q);
+        }
+        
+        
     }
     
 }
